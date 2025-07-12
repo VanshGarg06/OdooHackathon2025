a6708eacee6d8d9a12ba779669ce5255c0c65232
Rishabh <rishabhjain61002@gmail.com>
2025-07-12 15:31:42 +0530

    PHP Added

--- answers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-
-urlpatterns = [
-    path('<int:id>/vote/', VoteAnswerView.as_view()),  # POST upvote/downvote
-    path('<int:id>/', AnswerDetailView.as_view()),     # optional for edit/delete
-]

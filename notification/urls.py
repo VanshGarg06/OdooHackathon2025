10a37334abbb41a9a8e0d8fcdebc2502522c9e08
Rishabh <rishabhjain61002@gmail.com>
2025-07-12 15:31:24 +0530

    PHP Added

--- notification/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-urlpatterns = [
-    path('', NotificationListView.as_view()),
-    path('mark-as-read/', MarkAllReadView.as_view()),
-]
